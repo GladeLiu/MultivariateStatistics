0e13d7e0890d5339bcf64fdd029e902117c71da3
Chenang Liu <liuchenang@gmail.com>
2014-01-22 22:44:40 +0800

    the statistic in hierarchical clustering
    the statistic in hierarchical clustering which can help us to determine
    the number of clusters

---
+++ ClusterNum.R
@@ -0,0 +1,43 @@
+ClusterNum<-function(X,d="euclidean",method="ward"){
+  X<-data.frame(scale(X));
+  d<-dist(X,d);
+  hc<-hclust(d,method);
+  N<-dim(X)[1];
+  R2<-matrix(seq(0,1,length=N),ncol=1,nrow=N);
+  PSF<-matrix(0,ncol=1,nrow=N);
+  PST2<-matrix(0,ncol=1,nrow=N);
+  T<-sum(diag(cov(X)*(N-1)));
+  Nm<-rep(0,N-1);Wm<-rep(0,N-1);
+  pdf("cluster");plot(1,type="n");
+  rect0<-rect.hclust(hc,k=2);
+  for(t in 2:(N-1)){
+    rect<-rect0;
+    if(t<N-1){
+       rect0<-rect.hclust(hc,k=t+1);
+    }
+    b<-colMeans(X[rect[[1]],])-colMeans(X);
+    B<-length(rect[[1]])*t(b)%*%b;
+    for(j in 1:t){
+        if(length(rect[[j]])!=length(rect0[[j]])){
+        break;}
+      }
+    Nm[t]<-length(rect[[j]]);
+    Wm[t]<-sum(diag(cov(X[rect[[j]],])*(length(rect[[j]])-1)));
+    for(i in t:2){
+      b<-colMeans(X[rect[[i]],])-colMeans(X);
+      B<-B+length(rect[[i]])*t(b)%*%b;
+    }
+   R2[t]<-B/T;
+   PSF[t]<-(B*(N-t))/((T-B)*(t-1));
+  }
+  R1<-R2[-1];SPRSQ<-matrix(R1-R2[1:(N-1)]);
+  Bkl<-SPRSQ*T;
+  PST2<-(Nm-2)*Bkl/(Wm-Bkl);
+  PST2[1]<-(N-2)*Bkl[1]/(T-Bkl[1]);
+  PST2[c(N-1,N-2)]<-0;
+  data.frame(RSQ=R2[1:(N-1)],SPRSQ,PSF=PSF[1:(N-1)],PST2);
+}
+
+#a simple example
+data(USArrests);
+ClusterNum(USArrests);
