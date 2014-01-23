e2878cde7bec2499358e7a83731ed5b6d201dfa6
Chenang Liu <liuchenang@gmail.com>
2014-01-23 22:58:02 +0800

    the number of clusters
    A R function which can help us to determine the number of clusters

--- ClusterNum.R
+++ ClusterNum.R
@@ -1,11 +1,18 @@
-ClusterNum<-function(X,d="euclidean",method="ward"){
-  X<-data.frame(scale(X));
-  d<-dist(X,d);
-  hc<-hclust(d,method);
-  N<-dim(X)[1];
-  R2<-matrix(seq(0,1,length=N),ncol=1,nrow=N);
-  PSF<-matrix(0,ncol=1,nrow=N);
-  PST2<-matrix(0,ncol=1,nrow=N);
+ClusterNum<-function(X,d="euclidean",method="ward",hclust=NULL){
+#X is required
+  if(class(hclust)=="NULL"){
+     X<-data.frame(scale(X));
+     d<-dist(X,d);
+     hc<-hclust(d,method);
+     N<-dim(X)[1];
+  }else if(class(hclust)=="hclust"){
+     hc<-hclust;
+     N<-length(hc$order);
+  }else{
+     stop("The class of hclust is wrong!");
+  }  
+  R2<-seq(0,1,length=N);
+  PSF<-rep(0,N);PST2<-PSF;
   T<-sum(diag(cov(X)*(N-1)));
   Nm<-rep(0,N-1);Wm<-rep(0,N-1);
   pdf("cluster");plot(1,type="n");
@@ -30,6 +37,7 @@ ClusterNum<-function(X,d="euclidean",method="ward"){
    R2[t]<-B/T;
    PSF[t]<-(B*(N-t))/((T-B)*(t-1));
   }
+  dev.off();
   R1<-R2[-1];SPRSQ<-matrix(R1-R2[1:(N-1)]);
   Bkl<-SPRSQ*T;
   PST2<-(Nm-2)*Bkl/(Wm-Bkl);
